5a3b4cae2ca116372b44a546e42c44dfafc22d00
VitaxaOv <vit2013aly@mail.ru>
2022-11-13 15:01:53 +0300

    add recursive and axample

---
+++ recursive/Program.cs
@@ -0,0 +1,148 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/* Задача 45: Напишите программу, которая 
+будет создавать копию заданного массива с помощью
+поэлементного копирования. */
+
+/*int[] array = new int[10];
+FillArray(array);
+Console.WriteLine("Initial");
+PrintArray(array);
+Console.WriteLine();
+int[] copyArray = array;//CopyArray(array);
+array[0] = 12312312;
+Console.WriteLine("Initial changed");
+PrintArray(array);
+//CopyArray(array);
+Console.WriteLine("Copied");
+PrintArray(copyArray);
+
+void FillArray(int[] array)
+{
+    Random random = new Random();
+    for (var i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 11);
+    }
+}
+int[] CopyArray(int[] array)
+{
+    int[] copyArray = new int[array.Length];
+
+    for (var i = 0; i < array.Length; i++)
+    {
+        copyArray[i] = array[i];
+    }
+    return copyArray;
+}
+
+void PrintArray(int[] array)
+{
+    for (var i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+     Console.WriteLine();
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+double FibonacciBad(int n)
+{
+	if(n == 1 || n == 2) return 1;
+	else return FibonacciBad(n-1) + FibonacciBad(n-2);
+}
+
+double FibonacciR(double[] f, int n)
+{
+	if(n == 1 || n == 2) 
+    {
+        return f[n-1] = 1;
+    }
+	else
+    {
+        if(f[n-2]!=0)
+        {
+            if(f[n-1]!=0)
+                return f[n-1] = f[n-2] + f[n-3];
+            else
+                return  f[n-1] = FibonacciR(f, n-1) + f[n-3];
+        }
+        else
+            return f[n-1] = FibonacciR(f, n-1) + FibonacciR(f, n-2);
+    }
+}
+double FibonacciGood(int n)
+{
+    
+    double[] f = new double[n];
+    return FibonacciR(f, n);
+}
+
+
+int countTests = 40;
+
+
+DateTime saveTime;
+
+Console.WriteLine($"Цикл for: ");
+saveTime = DateTime.Now;
+int fibonacci1 = 1;
+int fibonacci2 = 1;
+for (int i = 1; i <= countTests; i++)
+{
+	Console.WriteLine($"f({i}) = {fibonacci2}");
+	int temporary = fibonacci2;
+	fibonacci2 += fibonacci1;
+	fibonacci1 = temporary; 
+}
+Console.WriteLine($"Для for прошло: {DateTime.Now - saveTime}");
+
+
+Console.WriteLine($"FibonacciBad: ");
+saveTime = DateTime.Now;
+for (int i = 1; i <= countTests; i++)
+{
+
+	Console.WriteLine($"F({i}) = {FibonacciBad(i)}");
+}
+Console.WriteLine($"Для FibonacciBad прошло: {DateTime.Now - saveTime}");
+
+
+Console.WriteLine($"FibonacciGood: ");
+saveTime = DateTime.Now;
+for (int i = 1; i <= countTests; i++)
+{
+
+	Console.WriteLine($"F({i}) = {FibonacciGood(i)}");
+}
+Console.WriteLine($"Для FibonacciR прошло: {DateTime.Now - saveTime}");
